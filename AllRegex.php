b56f95b660a7cf9e32ab36e354aee4311a0c8aa9
Dalveer Singh <dalvirsahota@gmail.com>
2016-10-01 23:01:46 -0400

    version 1

---
+++ AllRegex.php
@@ -0,0 +1,8 @@
+<?php
+
+class AllRegex
+{
+    public static $EMAILREGEX = "/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/";
+    public static $PHONEREGEX = "/^[1-9]{1}[0-9]{2}-[0-9]{3}-[0-9]{4}$/";
+    public static $PASSWORDREGEX = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/";
+}
--- Validator.php
+++ Validator.php
@@ -1,12 +1,34 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: damil
- * Date: 2016-09-29
- * Time: 11:24 AM
- */
 class Validator
 {
+    private $values;
+    private $error;
 
+    public function __costruct($values){
+        $this->values = $values;
+        $this->error = "";
+}
+function validation() {
+    global $values,$error;
+    foreach ($values as $key=>$value){
+        if($key=='email'){
+             if(!preg_match(AllRegex::$EMAILREGEX,$value)){
+              $error = "Email is not valid please enter again<br>";
+             }
+        }
+        if($key =='phone'){
+            if(!preg_match(AllRegex::$PHONEREGEX,$value)){
+                $error = "Phone is not valid please enter again<br>";
+            }
+        }
+        if($key =='password'){
+            if(!preg_match(AllRegex::$PASSWORDREGEX,$value)){
+                $error = "Password is not valid please enter again<br>";
+            }
+        }
+
+    }
+    return $error;
+}
 }
