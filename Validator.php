b3717094bfe0805a4c4789d3f2707b200925a180
Dalveer Singh <dalvirsahota@gmail.com>
2016-10-01 23:18:49 -0400

    version 1 detailed

--- Validator.php
+++ Validator.php
@@ -2,7 +2,9 @@
 
 class Validator
 {
+    //$values is an array that user will pass while creating object then it would be $_POST
     private $values;
+    //message that will respond with appropriate message
     private $error;
 
     public function __costruct($values){
@@ -12,6 +14,7 @@ class Validator
 function validation() {
     global $values,$error;
     foreach ($values as $key=>$value){
+        //iterating over $_POST and finding fields
         if($key=='email'){
              if(!preg_match(AllRegex::$EMAILREGEX,$value)){
               $error = "Email is not valid please enter again<br>";
