11168e66c25182dd3b01e1f30a264354c8235edc
Damil Panchal <damil51981@gmail.com>
2016-10-03 18:44:12 -0400

    created a function to check the input against input type

--- AllRegex.php
+++ AllRegex.php
@@ -5,4 +5,6 @@ class AllRegex
     public static $EMAILREGEX = "/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/";
     public static $PHONEREGEX = "/^[1-9]{1}[0-9]{2}-[0-9]{3}-[0-9]{4}$/";
     public static $PASSWORDREGEX = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/";
+    public static $DATEREGEX = "^[0-9]{4}[-/][0-9]{1,2}[-/][0-9]{1,2}/$";
+
 }
--- Validator.php
+++ Validator.php
@@ -7,6 +7,17 @@ class Validator
     //message that will respond with appropriate message
     private $error;
 
+
+    public static $regexs = Array(
+
+        'EMAILREGEX' => "/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/",
+        'PHONEREGEX' => "/^[1-9]{1}[0-9]{2}-[0-9]{3}-[0-9]{4}$/",
+        'PASSWORDREGEX' => "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/",
+        'DATEREGEX' => '^[0-9]{4}[-/][0-9]{1,2}[-/][0-9]{1,2}/$',
+        'POSTALCODEREGEX' => '[A-Z][0-9][A-Z] {2}',
+
+    );
+    
     public function __costruct($values){
         $this->values = $values;
         $this->error = "";
@@ -30,8 +41,47 @@ function validation() {
                 $error = "Password is not valid please enter again<br>";
             }
         }
+        if ($key == 'date') {
+            if (!preg_match(AllRegex::$DATEREGEX, $value)) {
+                $error .= "Date is not valid<br>";
+            }
+        }
+
 
     }
     return $error;
 }
+
+
+// function that will check for an input with a corresponding type and return a message if the value doesnt match
+
+// this function works on an array of regexs
+
+    public static function validate($input, $type)
+    {
+        if (array_key_exists($type, self::$regexs)) {
+            $returnval = filter_var($input, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => self::$regexs[$type]))) !== false; // function takes an associative array of options for input types which in turn is our array of regexs
+            return ($returnval);
+        }
+
+        $filter = false;
+        switch ($type) {
+            case 'email':
+                $filter = FILTER_VALIDATE_EMAIL;
+                break;
+            case 'int':
+                $filter = FILTER_VALIDATE_INT;
+                break;
+            case 'url':
+                $filter = FILTER_VALIDATE_URL;
+                break;
+        }
+        if ($filter === false) {
+            return false;
+        } else if (filter_var($input, $filter)) {
+            return true;
+        } else
+            return false;
+    }
+
 }
